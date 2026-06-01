862b6245a1bc8ad2aa56b7ac596159dd555e7f6b
Swastik <swastik.patro02@gmail.com>
2026-06-01 17:20:19 +0530

    feat: Subscription endpoint with test

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,19 @@ use std::net::TcpListener;
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()  //OK() gives httpresponseBuilder but we need response so to return response with empty body => .finish() but ok() also implements Responder so works both ways
 }
+#[derive(serde::Deserialize)]
+struct FormData{
+    name: String,
+    email: String
+}
 
+
+//Handlers
+// this form acts as middleware where it checks all params in the FormData are there or not otherswise the 2nd test case pass with 400
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse{ //currently it returns 200 for every req but the 2nd testcase requires it to pass with 400.
+    println!("HANDLER CALLED");
+    HttpResponse::Ok().finish()
+}
 
 
 //if u are not awaiting then remove the async also otherwise this will return Future<Result<T,E>>
@@ -16,7 +28,7 @@ pub fn run(listener : TcpListener)-> Result<Server, std::io::Error> { // now it 
         App::new()
             
             .route("/health_check", web::get().to(health_check))
-           
+            .route("/subscription", web::post().to(subscribe))
     })
     .listen(listener)? //bind returns result 
     .run()   ;          // .run() returns a Server type which you can think of a Future which will run by .await
