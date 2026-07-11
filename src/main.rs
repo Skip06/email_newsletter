f490a1e2b76b7783ccd12ea720a69c9942ed1d54
Swastik <swastik.patro02@gmail.com>
2026-07-11 20:58:37 +0530

    feat: added actix's log instrument listener using tracing-log crate and main restructuring fn

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,28 @@ use sqlx::{PgPool};
 use std::net::TcpListener;
                                                                                                 //use env_logger::Env;
 
-use tracing::subscriber::set_global_default;
+use tracing::{Subscriber, subscriber::set_global_default};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer; // this is for actix's internal log actix's instrument still uses log crate but we removed env_logger() so no one to listen for that  so we need tracing-log crate for log -> tracing
 
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error>{
-   // // run().await? // wrong cause Tries to `.await` the `Result<Server, Error>` — **wrong**, `Result` is not a future
-   //  run("http://localhost:8000")?.await    //?` unwraps the `Result` → gives a `Server`, then `.await` runs the server future — **correct
+/// We are using `impl Subscriber` as return type to avoid having to
+/// spell out the actual type of the returned subscriber, which is
+/// indeed quite complex.
+/// We need to explicitly call out that the returned subscriber is
+/// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
+/// later on.
 
-   // `init` does call `set_logger`, so this is all we need to do.
-   // We are falling back to printing all logs at info-level or above equivalent ,to RUST_LOG=info cargo run
-   // if the RUST_LOG environment variable has not been set.
+
+
+
+
                                                                                           //env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-   let env_filter = EnvFilter::try_from_default_env()
+   pub fn get_subscriber(
+      name: String,
+      env_filter: String,
+   )-> impl Subscriber + Send + Sync{
+      let env_filter = EnvFilter::try_from_default_env()
             .unwrap_or_else(|_| EnvFilter::new("info"));
 
    let formatting_layer = BunyanFormattingLayer::new(
@@ -28,14 +36,28 @@ async fn main() -> Result<(), std::io::Error>{
 
       // The `with` method is provided by `SubscriberExt`, an extension
    // trait for `Subscriber` exposed by `tracing_subscriber`
-   let subscriber = Registry::default()
+   let subscriber = Registry::default()   //subscriber is of now 3 layers wrapped but we want Registry which implements Subscriber trait so just return type impl Subscriber
    .with(env_filter)
    .with(JsonStorageLayer)
    .with(formatting_layer);
-   // `set_global_default` can be used by applications to specify
-   // what subscriber should be used to process spans.
-   set_global_default(subscriber).expect("Failed to set subscriber");
 
+   return subscriber;
+   }
+
+/// Register a subscriber as global default to process span data.
+///
+/// It should only be called once!
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+   LogTracer::init().expect("Failed to set logger");
+   set_global_default(subscriber).expect("Failed to set subscriber");
+   }
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error>{
+   let subscriber = get_subscriber("actixweb_email_newsletter".into(), "info".into());
+   init_subscriber(subscriber);
 
    let configuration = get_configuration().expect("could not read configs");
    let connection_pool = PgPool::connect(&configuration.database.connection_string()).await.expect("could not connect to postgres");
