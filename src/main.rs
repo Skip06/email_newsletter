db0c987909bd083c4bf31055b44c00a11a72b29c
Swastik <swastik.patro02@gmail.com>
2026-07-11 20:06:42 +0530

    json telemetry output feat: Enhance logging with tracing-bunyan-formatter and tracing-subscriber
    Replaced `env_logger` with `tracing-bunyan-formatter` for structured logging. Integrated `tracing-subscriber` to manage logging layers and improve log formatting.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use actixweb_email_newsletter::startup::run;
 use actixweb_email_newsletter::configuration::get_configuration;
 use sqlx::{PgPool};
 use std::net::TcpListener;
-use env_logger::Env;
+                                                                                                //use env_logger::Env;
+
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error>{
@@ -12,8 +16,27 @@ async fn main() -> Result<(), std::io::Error>{
    // `init` does call `set_logger`, so this is all we need to do.
    // We are falling back to printing all logs at info-level or above equivalent ,to RUST_LOG=info cargo run
    // if the RUST_LOG environment variable has not been set.
-   env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-   
+                                                                                          //env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+   let env_filter = EnvFilter::try_from_default_env()
+            .unwrap_or_else(|_| EnvFilter::new("info"));
+
+   let formatting_layer = BunyanFormattingLayer::new(
+      "actixweb_email_newsletter".into(),
+      // Output the formatted spans to stdout.
+      std::io::stdout
+   );
+
+      // The `with` method is provided by `SubscriberExt`, an extension
+   // trait for `Subscriber` exposed by `tracing_subscriber`
+   let subscriber = Registry::default()
+   .with(env_filter)
+   .with(JsonStorageLayer)
+   .with(formatting_layer);
+   // `set_global_default` can be used by applications to specify
+   // what subscriber should be used to process spans.
+   set_global_default(subscriber).expect("Failed to set subscriber");
+
+
    let configuration = get_configuration().expect("could not read configs");
    let connection_pool = PgPool::connect(&configuration.database.connection_string()).await.expect("could not connect to postgres");
    let address_port = configuration.app_port;
