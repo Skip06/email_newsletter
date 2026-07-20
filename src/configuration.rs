992448d7a4d84d3827af565062bdf079640bc648
Swastik <swastik.patro02@gmail.com>
2026-07-20 22:10:17 +0530

    feat: testing error in CD

--- src/configuration.rs
+++ src/configuration.rs
@@ -18,6 +18,10 @@ and opens a permanent line of communication to PostgreSQL so it can save those i
 use serde::Deserialize;
 use secrecy::Secret;
 use secrecy::ExposeSecret;
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::PgSslMode;
+use sqlx::ConnectOptions;
 
 #[derive(Deserialize)]
 pub struct Settings {
@@ -34,23 +38,35 @@ pub host: String,
 #[derive(Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub database_name: String,
     pub password: Secret<String>,
     pub host: String,
+    pub require_ssl: bool,
 }
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password.expose_secret(), self.host, self.port, self.database_name
-        ))
+    // Renamed from `connection_string`
+    pub fn with_db(&self) -> PgConnectOptions {
+        let mut options = self.without_db().database(&self.database_name);
+        options = options.log_statements(tracing_log::log::LevelFilter::Trace);
+        options
     }
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username, self.password.expose_secret(), self.host, self.port
-        ))
+
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } 
+        else {
+            // Try an encrypted connection, fallback to unencrypted if it fails
+            PgSslMode::Prefer
+        };
+        PgConnectOptions::new()
+        .host(&self.host)
+        .username(&self.username)
+        .password(&self.password.expose_secret())
+        .port(self.port)
+        .ssl_mode(ssl_mode)
     }
 }
 //
@@ -73,6 +89,14 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(config::File::from(
             configuration_dir.join(environment_filename)
         ))
+            // Add in settings from environment variables (with a prefix of APP and
+            // '__' as separator)
+            // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+        .add_source(
+            config::Environment::with_prefix("APP")
+            .prefix_separator("_")
+            .separator("__")
+            )
         .build()?; //now execute read, parse
     settings.try_deserialize::<Settings>()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use actixweb_email_newsletter::configuration::get_configuration;
 use actixweb_email_newsletter::startup::run;
 use actixweb_email_newsletter::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::{PgPool};
 use std::net::TcpListener;
 
@@ -13,9 +14,19 @@ use std::net::TcpListener;
 async fn main() -> Result<(), std::io::Error>{
    let subscriber = get_subscriber("actixweb_email_newsletter".into(), "info".into(), std::io::stdout);
    init_subscriber(subscriber);
+   
 
    let configuration = get_configuration().expect("could not read configs");
-   let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string().expose_secret()).expect("could not connect to postgres");
+   println!("HOST = {}", configuration.database.host);
+println!("PORT = {}", configuration.database.port);
+println!("USER = {}", configuration.database.username);
+println!("DB   = {}", configuration.database.database_name);
+println!("SSL  = {}", configuration.database.require_ssl);
+println!(
+    "ENV = {}",
+    std::env::var("APP_ENVIRONMENT").unwrap_or_else(|_| "NOT SET".into())
+);
+   let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());// `connect_lazy_with` instead of `connect_lazy`
    let address = format!("{}:{}",configuration.application.host, configuration.application.port);
    let listener = TcpListener::bind(address)?;
    run(listener,connection_pool)?.await
